cb231175b8c7e902056ffca78c6267c3fa013f58
Akarsh Srivastav <sriakarsh2003@gmail.com>
2022-12-01 01:07:47 +0530

    SideBar Intial Stage

---
+++ portfolio/src/components/Home/index.js
@@ -0,0 +1,13 @@
+
+
+
+const Home =()=>{
+
+    return (
+        <div>
+        hiome
+        </div>
+    )
+}
+
+export default Home;
---
+++ portfolio/src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import SideBar from '../SideBar';
+import {Box} from '@mui/material';
+
+const Layout =()=>{
+
+
+    return <>
+            <SideBar/>
+        </>
+}
+
+export default Layout;
